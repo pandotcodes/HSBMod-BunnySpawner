16d70fc589907fad7c99d8ccf93592033fe99a35
Jonathan Riedel <pn@bptv.eu>
2025-06-08 21:40:24 +0200

    Code updates

---
+++ ControllerFixer/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ControllerFixer
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+    [HarmonyPatch(typeof(SteamUtils), "IsSteamDeck")]
+    public static class SteamUtils_IsSteamDeck_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
